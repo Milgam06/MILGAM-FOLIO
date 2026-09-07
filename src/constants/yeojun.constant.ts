e53aed03c679518750af61a47b1c322982e03fc5
MILGAM <93873127+Milgam06@users.noreply.github.com>
2026-09-07 21:15:05 +0900

    feat(front): add yeojun_constant file to auto_update

---
+++ src/constants/yeojun.constant.ts
@@ -0,0 +1,19 @@
+type IYeojunConstant = {
+  name: string;
+  en_name: string;
+  alias: string;
+  birth: string;
+  role: string[];
+  email: string;
+  github: string;
+};
+
+export const YEOJUN_CONSTANT: IYeojunConstant = {
+  name: '박여준',
+  en_name: 'Park Yeojun',
+  alias: 'MILGAM',
+  birth: '2006-08-06',
+  role: ['Frontend Developer', 'Backend Developer'],
+  email: 'milgamfruit@gmail.com',
+  github: 'https://github.com/milgam06',
+};
